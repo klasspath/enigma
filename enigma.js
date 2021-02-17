4261a2512e934906f8ddfeecd6080d5e5807fb9d
Theo <69400012+Teschl@users.noreply.github.com>
2021-02-17 17:24:58 +0000

    Enigma js erbaut

--- enigma.js
+++ enigma.js
@@ -1,3 +1,19 @@
-export function divide(a, b) {
-    return a / b
+function main(){
+    let eingabe = 0;
+    let ausgabe = 0;
+    let ergebnis = "Error";
+    let alphabet = ["A","B","C","D","E","F","G","H","I"];   //hier ABC ergänzen
+
+    //Hier werden Zahlen in Buchstaben umgewandelt
+    for(i = 0; i<26; i++){
+        if(i == ausgabe){
+            ergebnis = alphabet[i]
+            i =26;
+        }
+    }
+    return ergebnis
 }
+function drei_trommeln (eingabe, ausgabe){
+    let trommel_1 = 
+}
+main()
