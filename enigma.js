e5c02fad421239e7463668850b52ed972ab585f2
Theo <69400012+Teschl@users.noreply.github.com>
2021-02-17 17:29:28 +0000

    update

--- enigma.js
+++ enigma.js
@@ -3,17 +3,12 @@ function main(){
     let ausgabe = 0;
     let ergebnis = "Error";
     let alphabet = ["A","B","C","D","E","F","G","H","I"];   //hier ABC ergänzen
-
+    drei_trommeln(eingabe, ausgabe);
     //Hier werden Zahlen in Buchstaben umgewandelt
-    for(i = 0; i<26; i++){
-        if(i == ausgabe){
-            ergebnis = alphabet[i]
-            i =26;
-        }
-    }
-    return ergebnis
+    ergebnis = alphabet[ausgabe]
 }
 function drei_trommeln (eingabe, ausgabe){
-    let trommel_1 = 
+    let trommel_1 = [1,6,3,7,2,5,4,9,10,8]  //erstmal nur bis 10
+    let trommel_1_eingang = 0;
 }
 main()
