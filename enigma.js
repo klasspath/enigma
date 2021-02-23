21eb40fb2ea318b58a7508708ad1db76e776f130
Theo <69400012+Teschl@users.noreply.github.com>
2021-02-23 10:16:32 +0000

    rtation

--- enigma.js
+++ enigma.js
@@ -3,7 +3,7 @@ export function main(){
     let output = 0;
     let result = "Error";
     let position = 0; // welcher Buchstabe
-    let displacement = [0,0,0]
+    let displacement = [4,15,7]
     output = encryption(input, output, position, displacement);
     
     console.log(output)
@@ -13,8 +13,8 @@ function encryption(input, output, position, displacement){
     let displacement_2 = 0;
     let displacement_3 = 0;
     let order_1 = [2,4,6,8,10,12,3,16,18,20,24,22,26,14,25,5,9,23,7,1,11,13,21,19,17,15];
-    let order_1 = [2,4,6,8,10,12,3,16,18,20,24,22,26,14,25,5,9,23,7,1,11,13,21,19,17,15];
-    let order_1 = [2,4,6,8,10,12,3,16,18,20,24,22,26,14,25,5,9,23,7,1,11,13,21,19,17,15];
+    let order_2 = [1,10,4,11,19,9,18,21,24,2,12,8,23,20,13,3,17,7,26,14,16,25,6,22,15,5];
+    let order_3 = [2,4,6,8,10,12,3,16,18,20,24,22,26,14,25,5,9,23,7,1,11,13,21,19,17,15];
     position = input;
     displacement_1 = displacemnet[0];
     displacement_2 = displacement[1];
@@ -34,10 +34,11 @@ function encryption(input, output, position, displacement){
     return output;
 }
 function barrel_1(position, order_1, displacement_1){
+    let rotation = 1;
     let temporary_position = 0;
-    temporary_position = order_1[position + displacement_1 + 1]; 
+    temporary_position = order_1[position + displacement_1 + rotation]; 
     // die Eingabe wird unter beachtung der verschiebung zur neuen Zahl gedreht
-    position = temporary_position + displacement_1 + 1;
+    position = temporary_position + displacement_1 + rotation;
     // Trommel ist rund, hier wenns rund geht ->
     if(position > 26){
         position = position - 26;
@@ -46,18 +47,20 @@ function barrel_1(position, order_1, displacement_1){
     return position;
 }
 function barrel_2(position, order_2, displacement_2){
+    let rotation = 0;
     let temporary_position = 0;
-    temporary_position = order_2[position + displacement_2 + 1]; 
-    position = temporary_position + displacement_2 + 1;
+    temporary_position = order_2[position + displacement_2 + rotation]; 
+    position = temporary_position + displacement_2 + rotation;
     if(position > 26){
         position = position - 26;
     }
     return position;
 }
 function barrel_3(position, order_3, displacement_3){
+    let rotation = 0;
     let temporary_position = 0;
-    temporary_position = order_3[position + displacement_3 + 1]; 
-    position = temporary_position + displacement_3 + 1;
+    temporary_position = order_3[position + displacement_3 + rotation]; 
+    position = temporary_position + displacement_3 + rotation;
     if(position > 26){
         position = position - 26;
     }
