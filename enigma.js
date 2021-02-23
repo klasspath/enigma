9fd3b840814a90c227dcb51c791c0f06f307d6c4
Theo <69400012+Teschl@users.noreply.github.com>
2021-02-23 11:09:28 +0000

    update

--- enigma.js
+++ enigma.js
@@ -3,7 +3,7 @@ export function main(){
     let output = 0;
     let result = "Error";
     let position = 0; // welcher Buchstabe
-    let displacement = [4,15,7];
+    let displacement = [6,14,3]; // -1 da start bei null
     output = encryption(input, output, position, displacement);
     
     console.log(output)
@@ -12,6 +12,7 @@ function encryption(input, output, position, displacement){
     let displacement_1 = 0;
     let displacement_2 = 0;
     let displacement_3 = 0;
+    // wohin leiten die Trommeln weiter??? so:
     let order_1 = [2,4,6,8,10,12,3,16,18,20,24,22,26,14,25,5,9,23,7,1,11,13,21,19,17,15];
     let order_2 = [1,10,4,11,19,9,18,21,24,2,12,8,23,20,13,3,17,7,26,14,16,25,6,22,15,5];
     let order_3 = [2,4,6,8,10,12,3,16,18,20,24,22,26,14,25,5,9,23,7,1,11,13,21,19,17,15];
@@ -21,11 +22,9 @@ function encryption(input, output, position, displacement){
     displacement_3 = displacement[2];
      
     //durchlauf durch die Rollen (hin)
-    position = barrel_1(position, order_1, displacement_1);
-    console.log(position)
-    position = barrel_2(position, order_2, displacement_2);
-    console.log(position)
-    position = barrel_3(position, order_3, displacement_3);
+    position = barrel_1(position, order_1, displacement_1); 
+    //  position = barrel_2(position, order_2, displacement_2);
+    //  position = barrel_3(position, order_3, displacement_3);
     //durchlauf durch die Rollen (zurück)
 
         //fehlt noch zurüch muss anders als hin(?)
@@ -37,32 +36,11 @@ function encryption(input, output, position, displacement){
 }
 function barrel_1(position, order_1, displacement_1){
     let rotation = 1;
-    let temporary_position = 0;
-    temporary_position = order_1[position + displacement_1 + rotation]; 
-    // die Eingabe wird unter beachtung der verschiebung (rotation) zur neuen Zahl gedreht
-    position = temporary_position + displacement_1 + rotation;
-    // Trommel ist rund, hier wenns rund geht ->
-    if(position > 26){
-        position = position - 26;
-    }
-    // der neue Buchtsabe wird gefunden (verschiebung + buchstabe = position in der Trommel)
-    return position;
-}
-function barrel_2(position, order_2, displacement_2){
-    let rotation = 0;
-    let temporary_position = 0;
-    temporary_position = order_2[position + displacement_2 + rotation]; 
-    position = temporary_position + displacement_2 + rotation;
-    if(position > 26){
-        position = position - 26;
-    }
-    return position;
-}
-function barrel_3(position, order_3, displacement_3){
-    let rotation = 0;
-    let temporary_position = 0;
-    temporary_position = order_3[position + displacement_3 + rotation]; 
-    position = temporary_position + displacement_3 + rotation;
+    let letter = 0;
+    console.log(position)
+    console.log(displacement_1)
+    letter = order_1[position + displacement_1 + rotation - 1]; // -1 weil array bei null startet
+    position = letter - displacement_1 + rotation;
     if(position > 26){
         position = position - 26;
     }
