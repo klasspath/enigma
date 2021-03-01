1f4fd9209cba40b018bd61d14b05c9a2922b5548
Theo <69400012+Teschl@users.noreply.github.com>
2021-03-01 10:30:10 +0000

    last save

--- src/service.ts
+++ src/service.ts
@@ -5,6 +5,12 @@ const inputField = <HTMLInputElement>document.getElementById("input")
 const decodeButton = document.getElementById("decode-button")
 decodeButton.addEventListener("click", onDecodeButtonClicked)
 
+const inputField = <HTMLInputElement>document.getElementById("newinput")
+
+const newdecodeButton = document.getElementById("new-decode-button")
+decodeButton.addEventListener("click", onDecodeButtonClicked)
+
+
 function onDecodeButtonClicked() {
     main(inputField.value)
 }
