d3cc8587280a8b6090df712118aee7cff74d6aec
Theo <69400012+Teschl@users.noreply.github.com>
2021-03-02 10:57:07 +0000

    all gemer changes

--- src/enigma.ts
+++ src/enigma.ts
@@ -1,8 +1,14 @@
-export default function main(input: string) {
+/*
+Es fehlt:
+    - walze 2 und 3 weiter drehen
+    - possible 0 = a oder 1 = a denkfehler
+    - rückweg maybe wrong, has to be tested further
+*/
+
+export default function main(input: string, displacement: [number, number, number]) {
     let output = 0;
     //let result = "Error";
     let position = 0; // welcher Buchstabe
-    let displacement = [6, 14, 3]; // -1 da start bei null
     let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
     // Eingabe von buchstabe zu Zahl
     for(let i = 0; i<26; i++){
@@ -28,24 +34,34 @@ function encryption(output: number, position: number, displacement: number[]) {
     let order_1 = [2, 4, 6, 8, 10, 12, 3, 16, 18, 20, 24, 22, 26, 14, 25, 5, 9, 23, 7, 1, 11, 13, 21, 19, 17, 15];
     let order_2 = [1, 10, 4, 11, 19, 9, 18, 21, 24, 2, 12, 8, 23, 20, 13, 3, 17, 7, 26, 14, 16, 25, 6, 22, 15, 5];
     let order_3 = [2, 4, 6, 8, 10, 12, 3, 16, 18, 20, 24, 22, 26, 14, 25, 5, 9, 23, 7, 1, 11, 13, 21, 19, 17, 15];
+    let mirrorOrder = [25, 18, 21, 8, 17, 19, 12, 4, 16, 24, 14, 7, 15, 11, 13, 9, 5, 2, 6, 26, 3, 23, 22, 10, 1, 20]
     displacement_1 = displacement[0];
     displacement_2 = displacement[1];
     displacement_3 = displacement[2];
 
     position = barrel_1(position, order_1, displacement_1);
     console.log("barrel_1, position on: ", position)
-    /*
     position = barrel_2(position, order_2, displacement_2);
+    console.log("barrel_2, position on: ", position)
     position = barrel_3(position, order_3, displacement_3);
+    console.log("barrel_3, position on: ", position)
+    barrelMirror(position, mirrorOrder);
+    console.log("Mirror, position on: ", position)
     position = barrel_3(position, order_3, displacement_3);
+    console.log("barrel_3, position on: ", position)
     position = barrel_2(position, order_2, displacement_2);
+    console.log("barrel_2, position on: ", position)
     position = barrel_1(position, order_1, displacement_1);
-    */
-    //fehlt noch zurüch muss anders als hin(?)
+    console.log("barrel_1, position on: ", position)
+
 
     output = position
 
     return output;
+}
+function barrelMirror(position: number, mirrorOrder: number[]){
+    position = mirrorOrder[position];
+    return position;
 }
 
 //Barrels müssen für > und < 26 geprüft werden also alls neu POGGERS
@@ -61,53 +77,46 @@ function barrel_1(position: number, order_1: number[], displacement_1: number){
     
     positionOnEntry = order_1[findEntryPositionValue];
     position = positionOnEntry - displacement_1;
-    if(position < 0){
+    if(position < 1){
         position = position + 25;
     }else if(position > 25){
         position = position - 25;
     }
     return position;
 }
-/*function barrel_1(position: number, order_1: number[], displacement_1: number) {
-    displacement_1 = displacement_1 + 1; // verschiebung wird um 1 erweitert
-    let letter = 0;
-    letter = order_1[position + displacement_1 - 1]; // -1 weil array bei null startet
-    position = letter - displacement_1;
-    if (position > 26) {
-        position = position - 26;
-    }
-    return position;
-}*/
-
 function barrel_2(position: number, order_2: number[], displacement_2: number){
-    // rotation wann bei der zweiten?
-    let letter = 0;
-    let eingabe = 0;
-    if(position + displacement_2 - 1 > 25){
-        eingabe = position + displacement_2 - 27;
-    }else{
-        eingabe = position + displacement_2 - 1;
+    let findEntryPositionValue = 0;
+    let positionOnEntry = 0;
+
+    findEntryPositionValue = position + displacement_2 - 1; // -1 weil array bei null startet
+    if(findEntryPositionValue > 25){
+        findEntryPositionValue = findEntryPositionValue -25;
     }
-    letter = order_2[eingabe];
-    if(letter - displacement_2 < 0){
-        eingabe = letter - displacement_2 + 26;
-    }else{
-        eingabe = letter - displacement_2;
-    }
-    position = eingabe;
-    if (position > 26) {
-        position = position - 26;
+    
+    positionOnEntry = order_2[findEntryPositionValue];
+    position = positionOnEntry - displacement_2;
+    if(position < 1){
+        position = position + 25;
+    }else if(position > 25){
+        position = position - 25;
     }
     return position;
 }
-
 function barrel_3(position: number, order_3: number[], displacement_3: number){
-    // rotation wann bei der zweiten?
-    let letter = 0;
-    letter = order_3[position + displacement_3 - 1];
-    position = letter - displacement_3;
-    if (position > 26) {
-        position = position - 26;
+    let findEntryPositionValue = 0;
+    let positionOnEntry = 0;
+
+    findEntryPositionValue = position + displacement_3 - 1; // -1 weil array bei null startet
+    if(findEntryPositionValue > 25){
+        findEntryPositionValue = findEntryPositionValue -25;
+    }
+    
+    positionOnEntry = order_3[findEntryPositionValue];
+    position = positionOnEntry - displacement_3;
+    if(position < 1){
+        position = position + 25;
+    }else if(position > 25){
+        position = position - 25;
     }
     return position;
 }
