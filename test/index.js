9ed18584e85273f8534a7fc77eeca6d60567d5c4
Theo <69400012+Teschl@users.noreply.github.com>
2021-04-07 13:49:32 +0000

    h

--- test/index.js
+++ test/index.js
@@ -11,19 +11,11 @@ function numberToLetter(output) {
     return charCode;
 }
 exports.numberToLetter = numberToLetter;
-function greaterThan26(checkSum) {
-    if (checkSum > 26) {
-        checkSum = checkSum - 26;
-    }
-    return checkSum;
-}
-function runBarrelOne(position) {
-    var order = [0, 6, 5, 4, 3, 2, 1];
-    var displacement = 2; //muss noch aus Webseite gezogen werden!!!
-    //let order = [0, 2, 4, 6, 8, 10, 12, 3, 16, 18, 20, 24, 22, 26, 14, 25, 5, 9, 23, 7, 1, 11, 13, 21, 19, 17, 15];
+function runBarrel(position, order, displacement) {
     var temporaryPosition = position + displacement;
     if (temporaryPosition > order.length - 1) {
-        temporaryPosition = temporaryPosition - order.length + 1;
+        temporaryPosition = temporaryPosition - order.length + 1; //das +1 als auch das -1 weil am Anfang des Array 0 ist dieser alos eins zu lang ist, dass muss ausgeglichen werden!
+        console.log("fail");
     }
     for (var i = 0; i < order.length; i++) {
         if (temporaryPosition == order[i]) {
@@ -33,51 +25,50 @@ function runBarrelOne(position) {
     position = position - displacement;
     return position;
 }
-function runBarrelTwo(position) {
-    //let order = [0, 1, 10, 4, 11, 19, 9, 18, 21, 24, 2, 12, 8, 23, 20, 13, 3, 17, 7, 26, 14, 16, 25, 6, 22, 15, 5];
-    var order = [0, 4, 5, 6, 1, 2, 3];
-    return position;
-}
-function runBarrelThree(position) {
-    //let order = [0, 2, 4, 6, 8, 10, 12, 3, 16, 18, 20, 24, 22, 26, 14, 25, 5, 9, 23, 7, 1, 11, 13, 21, 19, 17, 15];
-    var order = [0, 4, 3, 2, 1, 6, 5];
-    return position;
-}
-function reverseBarrelOne(position) {
-    //let order = [0, 2, 4, 6, 8, 10, 12, 3, 16, 18, 20, 24, 22, 26, 14, 25, 5, 9, 23, 7, 1, 11, 13, 21, 19, 17, 15];
-    var order = [0, 6, 5, 4, 3, 2, 1];
-    position = order[position];
-    return position;
-}
-function reverseBarrelTwo(position) {
-    //let order = [0, 1, 10, 4, 11, 19, 9, 18, 21, 24, 2, 12, 8, 23, 20, 13, 3, 17, 7, 26, 14, 16, 25, 6, 22, 15, 5];
-    var order = [0, 4, 5, 6, 1, 2, 3];
-    position = order[position];
-    return position;
-}
-function reverseBarrelThree(position) {
-    //let order = [0, 2, 4, 6, 8, 10, 12, 3, 16, 18, 20, 24, 22, 26, 14, 25, 5, 9, 23, 7, 1, 11, 13, 21, 19, 17, 15];
-    var order = [0, 4, 3, 2, 1, 6, 5];
-    position = order[position];
+function reverseBarrel(position, order, displacement) {
+    var temporaryPosition = position + displacement;
+    if (temporaryPosition > order.length - 1) {
+        temporaryPosition = temporaryPosition - order.length + 1;
+        console.log("fail");
+    }
+    position = order[temporaryPosition];
+    position = position - displacement;
     return position;
 }
 function mirrorPosition(position) {
-    //let mirrorOrder = [0, 25, 18, 21, 8, 17, 19, 12, 4, 16, 24, 14, 7, 15, 11, 13, 9, 5, 2, 6, 26, 3, 23, 22, 10, 1, 20];
-    var mirrorOrder = [0, 1, 6, 2, 5, 3, 4];
+    var mirrorOrder = [0, 25, 18, 21, 8, 17, 19, 12, 4, 16, 24, 14, 7, 15, 11, 13, 9, 5, 2, 6, 26, 3, 23, 22, 10, 1, 20];
+    //let mirrorOrder = [0,1,6,2,5,3,4]
     position = mirrorOrder[position];
     return position;
 }
 function runBarells(position) {
-    position = 2;
+    //let order_One =   [0, 2, 4, 6, 8, 10, 12, 3, 16, 18, 20, 24, 22, 26, 14, 25, 5, 9, 23, 7, 1, 11, 13, 21, 19, 17, 15];
+    //let order_Two =   [0, 1, 10, 4, 11, 19, 9, 18, 21, 24, 2, 12, 8, 23, 20, 13, 3, 17, 7, 26, 14, 16, 25, 6, 22, 15, 5];
+    //let order_Three = [0, 25, 18, 21, 8, 17, 19, 12, 4, 16, 24, 14, 7, 15, 11, 13, 9, 5, 2, 6, 26, 3, 23, 22, 10, 1, 20];
+    var order_One = [0, 6, 5, 4, 3, 2, 1];
+    var order_Two = [0, 4, 5, 6, 1, 2, 3];
+    var order_Three = [0, 4, 3, 2, 1, 6, 5];
+    var displacement_One = 0;
+    var displacement_Two = 0;
+    var displacement_Three = 0;
+    position = 1;
     console.log("Start: " + position);
-    position = runBarrelOne(position);
-    /*position = runBarrelTwo(position);
-    position = runBarrelThree(position);
+    position = runBarrel(position, order_One, displacement_One);
+    position = runBarrel(position, order_Two, displacement_Two);
+    position = runBarrel(position, order_Three, displacement_Three);
     position = mirrorPosition(position);
-    position = reverseBarrelThree(position);
-    position = reverseBarrelTwo(position);
-    position = reverseBarrelOne(position);*/
-    console.log("Ende: " + position);
+    position = reverseBarrel(position, order_Three, displacement_Three);
+    position = reverseBarrel(position, order_Two, displacement_Two);
+    position = reverseBarrel(position, order_One, displacement_One);
+    console.log("zurück: " + position);
+    position = reverseBarrel(position, order_One, displacement_One);
+    position = reverseBarrel(position, order_Two, displacement_Two);
+    position = reverseBarrel(position, order_Three, displacement_Three);
+    position = mirrorPosition(position);
+    position = runBarrel(position, order_Three, displacement_Three);
+    position = runBarrel(position, order_Two, displacement_Two);
+    position = runBarrel(position, order_One, displacement_One);
+    console.log("ende: " + position);
     return position;
 }
 function encryption(displacement, position) {
@@ -85,4 +76,4 @@ function encryption(displacement, position) {
     return position;
 }
 encryption([1, 1, 1], 1);
-//console.log(encryption(position));**
+//console.log(encryption(position));
--- test/index.ts
+++ test/index.ts
@@ -8,19 +8,11 @@ export function numberToLetter(output: number): string {
     return charCode;
 }
 
-function greaterThan26(checkSum: number) {
-    if (checkSum > 26) {
-        checkSum = checkSum - 26;
-    }
-    return checkSum;
-}
-
-function runBarrel(position: number): number {
-    let displacement = 2    //muss noch aus Webseite gezogen werden!!!
-
+function runBarrel(position: number, order: number[], displacement): number {
     let temporaryPosition = position + displacement;
     if(temporaryPosition > order.length -1){
         temporaryPosition = temporaryPosition - order.length + 1; //das +1 als auch das -1 weil am Anfang des Array 0 ist dieser alos eins zu lang ist, dass muss ausgeglichen werden!
+        console.log("fail"+ position)
     }
     for (let i = 0; i < order.length; i++) {
         if (temporaryPosition == order[i]) {
@@ -31,35 +23,53 @@ function runBarrel(position: number): number {
     return position;
 }
 
-function reverseBarrel(position: number): number{
+function reverseBarrel(position: number, order: number[], displacement: number): number{
+    let temporaryPosition = position + displacement;
+    if(temporaryPosition > order.length -1){
+        temporaryPosition = temporaryPosition - order.length + 1;
+        console.log("fail")
+    }
+    position = order[temporaryPosition];
+    position = position - displacement;
     return position;
 }
 
-
 function mirrorPosition(position: number): number{
-    //let mirrorOrder = [0, 25, 18, 21, 8, 17, 19, 12, 4, 16, 24, 14, 7, 15, 11, 13, 9, 5, 2, 6, 26, 3, 23, 22, 10, 1, 20];
-    let mirrorOrder = [0,1,6,2,5,3,4]
+    let mirrorOrder = [0, 25, 18, 21, 8, 17, 19, 12, 4, 16, 24, 14, 7, 15, 11, 13, 9, 5, 2, 6, 26, 3, 23, 22, 10, 1, 20];
+    //let mirrorOrder = [0,1,6,2,5,3,4]
     position = mirrorOrder[position]
     return position;
 }
-function runBarells(position): number {
-    //let order = [0, 2, 4, 6, 8, 10, 12, 3, 16, 18, 20, 24, 22, 26, 14, 25, 5, 9, 23, 7, 1, 11, 13, 21, 19, 17, 15];
-    //let order = [0, 1, 10, 4, 11, 19, 9, 18, 21, 24, 2, 12, 8, 23, 20, 13, 3, 17, 7, 26, 14, 16, 25, 6, 22, 15, 5];
-    //let order = [0, 25, 18, 21, 8, 17, 19, 12, 4, 16, 24, 14, 7, 15, 11, 13, 9, 5, 2, 6, 26, 3, 23, 22, 10, 1, 20];
-    let order_One = [0,6,5,4,3,2,1];
-    let order_Two = [0,4,5,6,1,2,3];
-    let order_Three = [0,4,3,2,1,6,5];
 
-    position = 2
+function runBarells(position): number {
+    //let order_One =   [0, 2, 4, 6, 8, 10, 12, 3, 16, 18, 20, 24, 22, 26, 14, 25, 5, 9, 23, 7, 1, 11, 13, 21, 19, 17, 15];
+    //let order_Two =   [0, 1, 10, 4, 11, 19, 9, 18, 21, 24, 2, 12, 8, 23, 20, 13, 3, 17, 7, 26, 14, 16, 25, 6, 22, 15, 5];
+    //let order_Three = [0, 25, 18, 21, 8, 17, 19, 12, 4, 16, 24, 14, 7, 15, 11, 13, 9, 5, 2, 6, 26, 3, 23, 22, 10, 1, 20];
+    let order_One =   [0,6,5,4,3,2,1];
+    let order_Two =   [0,4,5,6,1,2,3];
+    let order_Three = [0,4,3,2,1,6,5];
+    let displacement_One = 0;
+    let displacement_Two = 0;
+    let displacement_Three = 0;
+    position = 1
+
     console.log("Start: "+position)
-    position = runBarrelOne(position);
-    position = runBarrelTwo(position);
-    position = runBarrelThree(position);
+    position = runBarrel(position, order_One, displacement_One);
+    position = runBarrel(position, order_Two, displacement_Two);
+    position = runBarrel(position, order_Three, displacement_Three);
     position = mirrorPosition(position);
-    position = reverseBarrelThree(position);
-    position = reverseBarrelTwo(position);
-    position = reverseBarrelOne(position);
-    console.log("Ende: "+position)
+    position = reverseBarrel(position, order_Three, displacement_Three);
+    position = reverseBarrel(position, order_Two, displacement_Two);
+    position = reverseBarrel(position, order_One, displacement_One);
+    console.log("zurück: "+position)
+    position = reverseBarrel(position, order_One, displacement_One);
+    position = reverseBarrel(position, order_Two, displacement_Two);
+    position = reverseBarrel(position, order_Three, displacement_Three);
+    position = mirrorPosition(position);
+    position = runBarrel(position, order_Three, displacement_Three);
+    position = runBarrel(position, order_Two, displacement_Two);
+    position = runBarrel(position, order_One, displacement_One);
+    console.log("ende: "+position)
     return position;
 }
 
